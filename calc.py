fbbaca3aa137cf4465bdc0c4d710f6511a5bac3b
Bennett Thomson <thomsonb@tcd.ie>
2022-12-24 18:34:08 +0000

    complete calculate_growth function

--- calc.py
+++ calc.py
@@ -2,15 +2,16 @@ import numpy as np
 
 class InvestmentCalc:
 
-    def __init__(self, principal: int, years: int, growth: int, frequency: str = 'monthly') -> None:
+    def __init__(self, principal: int, years: int, growth_pc: int, frequency: str = 'monthly') -> None:
         
         self.dd_division_constants = {'monthly': 96, 'yearly': 8}
         self.principal = self._is_valid_principal(principal)
         self.years = self._is_valid_years(years)
-        self.growth =  self._is_valid_growth(growth)
+        self.growth =  self._is_valid_growth(growth_pc)
         self.frequency = self._is_valid_frequency(frequency)
         self.periods = self._years_to_periods()
         self.dd_chunks = self._calculate_dd_chunks()
+        self.tax_rate = 0.41
         
     
     def _is_valid_principal(self, principal) -> int:
@@ -23,15 +24,15 @@ class InvestmentCalc:
             raise ValueError("years needs to be greater than zero")
         return years
 
-    def _is_valid_growth(self, growth) -> float:
-        if growth < 0:
+    def _is_valid_growth(self, growth_pc) -> float:
+        if growth_pc < 0:
             raise ValueError('growth needs to be type int greater than zero')
-        return growth/100
+        return growth_pc/100
 
     def _is_valid_frequency(self, frequency) -> str:
         if frequency.lower() not in self.dd_division_constants.keys():
-            raise ValueError("frequency not recognised")
-        return frequency
+            raise ValueError(f"frequency not recognised. Choose from {self.dd_division_constants.keys()}. Not case sensitive.")
+        return frequency.lower()
     
     def _years_to_periods(self) -> int:
         freq_dict = {'monthly':12, 'yearly':1}
@@ -45,18 +46,46 @@ class InvestmentCalc:
     def growth_formula(self) -> float:
 
         return 1+(self.growth/12)
+
+    def dd_tax(self, value, principal):
+        profit = value - principal
+        net_profit = profit * (1 - self.tax_rate)
+        return principal + net_profit
     
     def calculate_growth(self):
         '''Returns an array recording an investment's growth and taxation for a given
         period of time.
         
-        Run this in chunks of up to 8 years, then do the deemed disposal'''
+        Runs in chunks of up to 8 years, then does the deemed disposal.
+        
+        Taxes final value.'''
 
-        inv_array = np.full(self.periods, self.principal, dtype=float)
-        growth_array = np.array(range(1, self.periods + 1), dtype = float) 
+        
+        growth_array = np.array(range(1, self.periods + 1), dtype = float)  
 
-        x = self.growth_formula() ** growth_array 
-        return x * self.principal
+        new_principal = self.principal
+
+        master_array = np.array(self.principal, dtype=float)
+
+        for chunk in range(self.dd_chunks['dd_chunks']):
+            growth_array = (self.growth_formula() ** np.array(range(1,97))) * new_principal
+            
+            new_principal = self.dd_tax(growth_array[-1], new_principal)
+            
+            master_array = np.append(master_array, growth_array)
+
+        if self.dd_chunks['remainder'] > 0:
+            growth_array = (self.growth_formula() ** np.array(range(1, self.dd_chunks['remainder']+1))) * new_principal
+            
+            new_principal = self.dd_tax(growth_array[-1], new_principal)
+            
+            master_array = np.append(master_array, growth_array)
+            
+            final_value = self.dd_tax(growth_array[-1], new_principal)
+            
+            master_array[-1] = final_value
+
+        return master_array
 
     def build_matrix(self):
         '''Build a matrix that tracks each month's investment growth and taxation
@@ -64,15 +93,10 @@ class InvestmentCalc:
     
     
 
-
-        
-
-
-x=InvestmentCalc(years=9, growth=10, principal=1000, frequency='monthly')
+x=InvestmentCalc(years=33, growth_pc=10, principal=1000, frequency='monthly')
 
 
-print(x.frequency)
+print(f"Interest compounds {x.frequency}")
 print(x.periods)
-print(x.calculate_growth())
-print(x.periods_after_dd())
 print(x.dd_chunks)
+print(x.calculate_growth())
