aec97e19426d8ef9470430ecf7b4855b9dd45d7a
Bennett Thomson <thomsonb@tcd.ie>
2022-12-24 19:59:06 +0000

    complete build_matrix func

--- calc.py
+++ calc.py
@@ -3,14 +3,13 @@ import numpy as np
 class InvestmentCalc:
 
     def __init__(self, principal: int, years: int, growth_pc: int, frequency: str = 'monthly') -> None:
-        
         self.dd_division_constants = {'monthly': 96, 'yearly': 8}
         self.principal = self._is_valid_principal(principal)
         self.years = self._is_valid_years(years)
         self.growth =  self._is_valid_growth(growth_pc)
         self.frequency = self._is_valid_frequency(frequency)
         self.periods = self._years_to_periods()
-        self.dd_chunks = self._calculate_dd_chunks()
+        self.dd_chunks = self._calculate_dd_chunks(self.periods)
         self.tax_rate = 0.41
         
     
@@ -38,13 +37,12 @@ class InvestmentCalc:
         freq_dict = {'monthly':12, 'yearly':1}
         return self.years * freq_dict[self.frequency]
     
-    def _calculate_dd_chunks(self):
-        dd_chunks = self.periods // self.dd_division_constants[self.frequency]
-        remainder = self.periods % self.dd_division_constants[self.frequency]
+    def _calculate_dd_chunks(self, periods):
+        dd_chunks = periods // self.dd_division_constants[self.frequency]
+        remainder = periods % self.dd_division_constants[self.frequency]
         return {'dd_chunks':dd_chunks, 'remainder':remainder}
         
     def growth_formula(self) -> float:
-
         return 1+(self.growth/12)
 
     def dd_tax(self, value, principal):
@@ -52,7 +50,7 @@ class InvestmentCalc:
         net_profit = profit * (1 - self.tax_rate)
         return principal + net_profit
     
-    def calculate_growth(self):
+    def calculate_growth(self, periods):
         '''Returns an array recording an investment's growth and taxation for a given
         period of time.
         
@@ -60,22 +58,21 @@ class InvestmentCalc:
         
         Taxes final value.'''
 
-        
-        growth_array = np.array(range(1, self.periods + 1), dtype = float)  
-
         new_principal = self.principal
 
         master_array = np.array(self.principal, dtype=float)
 
-        for chunk in range(self.dd_chunks['dd_chunks']):
+        dd_chunks = self._calculate_dd_chunks(periods)
+
+        for chunk in range(dd_chunks['dd_chunks']):
             growth_array = (self.growth_formula() ** np.array(range(1,97))) * new_principal
             
             new_principal = self.dd_tax(growth_array[-1], new_principal)
             
             master_array = np.append(master_array, growth_array)
 
-        if self.dd_chunks['remainder'] > 0:
-            growth_array = (self.growth_formula() ** np.array(range(1, self.dd_chunks['remainder']+1))) * new_principal
+        if dd_chunks['remainder'] > 0:
+            growth_array = (self.growth_formula() ** np.array(range(1, dd_chunks['remainder']+1))) * new_principal
             
             new_principal = self.dd_tax(growth_array[-1], new_principal)
             
@@ -88,15 +85,34 @@ class InvestmentCalc:
         return master_array
 
     def build_matrix(self):
-        '''Build a matrix that tracks each month's investment growth and taxation
-        for a given period of time'''
+        '''
+        -Build a 2d array that tracks each month's investment growth and taxation
+        for a given period of time.
+        -use calculate_growth for each month's / year's investment, adjusting period var.
+        -Add zeros as prefix for each month. month 1 = 1 zero, month 2 = 2 zeros, etc
+        -Append to master_array
+        -Reshape at end into 2D array. Each period's array must be same shape before appending!!
+        '''
+        master_array = np.zeros(0,dtype=float)
+        for i in range(self.periods+1):
+            zeros_array = np.zeros(i, dtype = float)
+            growth_array = self.calculate_growth(periods = self.periods - i)
+            
+            result_array = np.append(zeros_array, growth_array)
+            print(result_array.shape)
+            master_array = np.append(result_array, master_array)
+        
+        return master_array.reshape(self.periods+1, self.periods+1)
+    
+    def matrix_sum(self):
+        return np.sum(self.build_matrix(), axis = 0)
     
     
 
-x=InvestmentCalc(years=33, growth_pc=10, principal=1000, frequency='monthly')
+x=InvestmentCalc(years=10, growth_pc=10, principal=1000, frequency='monthly')
 
 
 print(f"Interest compounds {x.frequency}")
 print(x.periods)
 print(x.dd_chunks)
-print(x.calculate_growth())
+print(x.matrix_sum())
